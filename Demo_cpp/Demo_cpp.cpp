c8b8c15c116764c22fa1d7985650690a9e9780ee
lileowang <lileowang@github.com>
2019-05-09 14:40:10 -0700

    added Test_boost_regex class to match string pattern with boost regex

--- Demo_cpp/Demo_cpp.cpp
+++ Demo_cpp/Demo_cpp.cpp
@@ -2,16 +2,22 @@
 // Start Date:  2019-05-09
 // Description:
 //      - Demo project for C/C++  features
+// Notes:
+//      - The individual features are implemented in separated classes.
 // Change history:
 //      - Refer to GitHub comments related to each source file.
 //
 
 #include "pch.h"
 #include <iostream>
+#include "Test_boost_regex.h"
 
 int main()
 {
     bool bRet = true;
+
+    Test_boost_regex test_boost_regex;
+    bRet = test_boost_regex.Run();
 
     printf("%s \n", bRet ? "pass" : "fail");
     getchar();
