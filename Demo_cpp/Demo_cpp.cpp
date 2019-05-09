5e0b6c9bf6302f847840987517e22542b3bb67bc
lileowang <lileowang@github.com>
2019-05-09 14:22:18 -0700

    created initial Visual C++ console solution in Visual Studio 2017

---
+++ Demo_cpp/Demo_cpp.cpp
@@ -0,0 +1,19 @@
+// Author:      Li Leo Wang
+// Start Date:  2019-05-09
+// Description:
+//      - Demo project for C/C++  features
+// Change history:
+//      - Refer to GitHub comments related to each source file.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    bool bRet = true;
+
+    printf("%s \n", bRet ? "pass" : "fail");
+    getchar();
+    return 0;
+}
